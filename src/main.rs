b8662c990b9d22b870cf19288f099ead8ba6478a
Julian Ceipek <julian@darklang.com>
2020-03-11 16:45:19 -0700

    clippy fix

--- src/main.rs
+++ src/main.rs
@@ -390,7 +390,7 @@ fn app() -> Result<(), DarkError> {
                         .unwrap_or("<Unknown>")
                         .to_string();
                     Err(DarkError::Non200Response(
-                        response.text().unwrap_or("<Undecodable>".to_string()),
+                        response.text().unwrap_or_else(|_| String::from("<Undecodable>")),
                         response.status().as_u16(),
                         exec_id,
                     ))
